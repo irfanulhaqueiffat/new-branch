11a6b2a9aad5526d2ad0ee199d72653ef2e164de
irfanulhaqueiffat <irfanifatcbx@gmail.com>
2025-10-16 09:54:50 +0600

    uss

---
+++ layot/layout.jsx
@@ -0,0 +1,4 @@
+ReadableStreamDefaultController.prototype.close = function close() {
+  if (!IsReadableStreamDefaultController(this)) {
+    throw new TypeError('Method ReadableStreamDefaultController.prototype.close can only be used on a ReadableStreamDefaultController');
+  }        
